fd4bf40570f534fd439942fd03becb0115b46a0e
Alejandro Mechine Gamarra <alejandromechinegamarra@gmail.com>
2017-10-05 14:10:11 +0200

    Terminado

--- app/src/main/java/com/example/mostrarweb/MostrarWeb.java
+++ app/src/main/java/com/example/mostrarweb/MostrarWeb.java
@@ -1,13 +1,38 @@
 package com.example.mostrarweb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MostrarWeb extends AppCompatActivity {
+public class MostrarWeb extends AppCompatActivity implements View.OnClickListener {
+    Button btnIr;
+    EditText etxNavegar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_web);
+
+        btnIr = (Button) findViewById(R.id.btnIr);
+        btnIr.setOnClickListener(this);
+        etxNavegar = (EditText) findViewById(R.id.etxNavegar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (etxNavegar.getText().toString().substring(0,7) != "http://" || etxNavegar.getText().toString().substring(0,8) != "https://") {
+            etxNavegar.setText("http://" + etxNavegar.getText().toString());
+        }
+        Bundle bundle = new Bundle();
+        bundle.putString("webMostrada", etxNavegar.getText().toString());
+        Intent intent = new Intent(this, WebMostrada.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+
+
     }
 }
--- app/src/main/java/com/example/mostrarweb/WebMostrada.java
+++ app/src/main/java/com/example/mostrarweb/WebMostrada.java
@@ -2,12 +2,19 @@ package com.example.mostrarweb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class WebMostrada extends AppCompatActivity {
+
+    WebView wbvWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_mostrada);
+
+        wbvWeb = (WebView)findViewById(R.id.wbvWeb);
+        wbvWeb.loadUrl(getIntent().getExtras().getString("webMostrada"));
+
     }
 }
